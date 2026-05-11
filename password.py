a3b583e46a0ef131bbd4fc820abde6f3ef401a92
Jay Patel <jaypatelpardi@gmail.com>
2026-05-11 15:51:19 +0530

    Added random password generation logic

--- password.py
+++ password.py
@@ -1,1 +1,18 @@
-print("Secure Password Generator Started")
+import random
+import string
+
+print("===== PASSWORD GENERATOR =====")
+
+password_length = int(input("Enter password length: "))
+
+all_characters = (
+    string.ascii_letters +
+    string.digits
+)
+
+generated_password = ""
+
+for i in range(password_length):
+    generated_password += random.choice(all_characters)
+
+print("Generated Password:", generated_password)
